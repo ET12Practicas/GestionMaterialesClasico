a701d1a06bbc1ed8d004ac218595106423535267
Prácticas Profesionalizantes ET12 <practicasprof12@gmail.com>
2017-09-03 01:05:53 -0300

    Create scriptUsuarios.sql

---
+++ scriptUsuarios.sql
@@ -0,0 +1,9 @@
+insert into rol (Nombre)
+values('Oficina Técnica'),
+      ('Depósito'),
+      ('Rectoria');   
+insert into usuario (Nombre, Contrasenia, Rol_Id, Rol_Id1)
+values('gonzalo', 'tecnica', 1, 1),
+      ('gustavo', 'tecnica', 1, 1),
+      ('jonathan', 'tecnica', 1, 1);
+      
