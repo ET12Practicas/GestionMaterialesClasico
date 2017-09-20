ee858f1f3f781de2fc29dc81d43efbaee144b016
Jonathan Velazquez <jonathanvgms@users.noreply.github.com>
2017-09-20 14:19:09 +0200

    Tipo material

---
+++ gestionmateriales/gestionmateriales/Models/GestionMateriales/tipoMaterial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace gestionmateriales.Models.GestionMateriales
+{
+    [Table("TipoMaterial")]
+    public class TipoMaterial
+    {
+        [Key]
+        [Required]
+        public int idTipoMaterial { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string nombre { get; set; }
+
+        public TipoMaterial()
+        {
+
+        }
+    }
+}
