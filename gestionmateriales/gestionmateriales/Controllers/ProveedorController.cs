d95fc8baba6bf15890b4ff3fc70e7129499e7d78
jvelazquez <jonathanvgms@gmail.com>
2018-08-20 13:48:25 -0300

    Refactor Controlador Proveedor

--- gestionmateriales/gestionmateriales/Controllers/ProveedorController.cs
+++ gestionmateriales/gestionmateriales/Controllers/ProveedorController.cs
@@ -9,6 +9,8 @@ using gestionmateriales.Repository.Implementation;
 
 namespace gestionmateriales.Controllers
 {
+    [Authorize(Roles = "administrador, oficinatecnica, compras")]
+    [Route("/Proveedor")]
     public class ProveedorController : Controller
     {
         private IProveedorRepository proveedorRepository;
@@ -18,166 +20,161 @@ namespace gestionmateriales.Controllers
             proveedorRepository = new ProveedorRepository(new OficinaTecnicaEntities());
         }
 
-        // GET: Proveedor
         [Authorize(Roles = "administrador, oficinatecnica, compras, rectoria")]
-        [Route("/Proveedor")]
         [HttpGet]
         public ActionResult Index()
         {
             return View("Index");
         }
 
-        [Authorize(Roles = "administrador, oficinatecnica, compras")]
-        [Route("/Proveedor/Agregar")]
         [HttpGet]
         public ActionResult Agregar()
         {
-            return View();
-        }
-
-        //POST: Proveedor/1/Agregar
-        [Authorize(Roles = "administrador, oficinatecnica, compras")]
-        [HttpPost]
-        public ActionResult Agregar(Proveedor unProveedor)
-        {
-            using (OficinaTecnicaEntities db = new OficinaTecnicaEntities())
-            {
-                if (db.proveedores.Any(y => y.cuit == unProveedor.cuit && y.hab))
-                {
-                    ViewBag.Result = 1;
-                    return View("Agregar", unProveedor);
-                }
-                try
-                {
-                    var p = new Proveedor();
-                    p.nombre = unProveedor.nombre;
-                    p.cuit = unProveedor.cuit;
-                    p.razonSocial = unProveedor.razonSocial;
-                    p.direccion = unProveedor.direccion;
-                    p.zona = unProveedor.zona;
-                    p.horario = unProveedor.horario;
-                    p.telefono = unProveedor.telefono;
-                    p.email = unProveedor.email;
-                    p.nombreContacto = unProveedor.nombreContacto;
-                    p.CREATED_BY = User.Identity.Name;
-                    p.CREATION_DATE = DateTime.Now;
-                    p.CREATION_IP = Request.UserHostAddress;
-                    p.LAST_UPDATED_BY = User.Identity.Name;
-                    p.LAST_UPDATED_DATE = DateTime.Now;
-                    p.LAST_UPDATED_IP = Request.UserHostAddress;
-                    db.proveedores.Add(p);
-                    db.SaveChanges();
-                }
-                catch
-                {
-                    return RedirectToAction("Error406", "Error");
-                }
-            }
-            ViewBag.Result = 0;
             return View("Agregar");
         }
 
-        //GET: Proveedor/Editar/1
-        [Authorize(Roles = "administrador, oficinatecnica, compras")]
-        [Route("/Proveedor/Editar/{id}")]
+        [HttpPost]
+        public ActionResult Agregar(Proveedor unProveedor)
+        {
+            if (proveedorRepository.GetAll().Any(y => y.cuit == unProveedor.cuit && y.hab))
+            {
+                ViewBag.Result = 1;
+
+                return View("Agregar", unProveedor);
+            }
+
+            try
+            {
+                proveedorRepository.Add(new Proveedor
+                {
+                    nombre = unProveedor.nombre,
+                    cuit = unProveedor.cuit,
+                    razonSocial = unProveedor.razonSocial,
+                    direccion = unProveedor.direccion,
+                    zona = unProveedor.zona,
+                    horario = unProveedor.horario,
+                    telefono = unProveedor.telefono,
+                    email = unProveedor.email,
+                    nombreContacto = unProveedor.nombreContacto,
+                    CREATED_BY = User.Identity.Name,
+                    CREATION_DATE = DateTime.Now,
+                    CREATION_IP = Request.UserHostAddress,
+                    LAST_UPDATED_BY = User.Identity.Name,
+                    LAST_UPDATED_DATE = DateTime.Now,
+                    LAST_UPDATED_IP = Request.UserHostAddress
+                });
+            }
+            catch
+            {
+                return RedirectToAction("Error406", "Error");
+            }
+
+            ViewBag.Result = 0;
+
+            return View("Agregar");
+        }
+
         [HttpGet]
         public ActionResult Editar(int id)
         {
-            Proveedor unProveedor;
-            using (OficinaTecnicaEntities db = new OficinaTecnicaEntities())
-            {
-                try
-                {
-                    unProveedor = db.proveedores.Find(id);
-                }
-                catch
-                {
-                    return RedirectToAction("Error406", "Error");
-                }
-            }
-            return View(unProveedor);
+            var proveedor = proveedorRepository.GetById(id);
+
+            if (proveedor == null) throw new Exception("No existe el proveedor");
+
+            return View("Editar", proveedor);
         }
 
-        //POST: Proveedor/Editar/1
-        [Authorize(Roles = "administrador, oficinatecnica, compras")]
         [HttpPost]
         public ActionResult Editar(int id, Proveedor unProveedor)
         {
-            var db = new OficinaTecnicaEntities();
-            Proveedor p = db.proveedores.Find(id);
-            if (db.proveedores.Where(x => x.idProveedor != id && x.hab).Any(y => y.cuit == unProveedor.cuit))
+            Proveedor proveedor = proveedorRepository.GetById(id);
+
+            if (proveedor == null) throw new Exception("No existe el proveedor");
+
+            if (proveedorRepository.Find(x => x.idProveedor != id && x.hab).Any(y => y.cuit == unProveedor.cuit))
             {
                 ViewBag.Result = 1;
+
                 return View("Editar", unProveedor);
             }
+
             try
             {
-                p.nombre = unProveedor.nombre;
-                p.cuit = unProveedor.cuit;
-                p.razonSocial = unProveedor.razonSocial;
-                p.horario = unProveedor.horario;
-                p.telefono = unProveedor.telefono;
-                p.nombreContacto = unProveedor.nombreContacto;
-                p.direccion = unProveedor.direccion;
-                p.LAST_UPDATED_BY = User.Identity.Name;
-                p.LAST_UPDATED_DATE = DateTime.Now;
-                p.LAST_UPDATED_IP = Request.UserHostAddress;
-                db.SaveChanges();
+                proveedor.nombre = unProveedor.nombre;
+
+                proveedor.cuit = unProveedor.cuit;
+
+                proveedor.razonSocial = unProveedor.razonSocial;
+
+                proveedor.horario = unProveedor.horario;
+
+                proveedor.telefono = unProveedor.telefono;
+
+                proveedor.nombreContacto = unProveedor.nombreContacto;
+
+                proveedor.direccion = unProveedor.direccion;
+
+                proveedor.LAST_UPDATED_BY = User.Identity.Name;
+
+                proveedor.LAST_UPDATED_DATE = DateTime.Now;
+
+                proveedor.LAST_UPDATED_IP = Request.UserHostAddress;
+
+                proveedorRepository.Edit(proveedor);
             }
             catch
             {
                 return RedirectToAction("Error406", "Error");
             }
+
             ViewBag.Result = 0;
+
             return View("Editar", unProveedor);
         }
 
-        //POST: Proveedor/Borrar/1
-        [Authorize(Roles = "administrador, oficinatecnica, compras")]
+        [HttpPost]
         public ActionResult Borrar(int id)
         {
-            var db = new OficinaTecnicaEntities();
-            Proveedor p = db.proveedores.Find(id);
+            Proveedor proveedor = proveedorRepository.GetById(id);
+
+            if (proveedor == null) throw new Exception("El proveedor no existe");
 
             try
             {
-                p.hab = false;
-                p.LAST_UPDATED_BY = User.Identity.Name;
-                p.LAST_UPDATED_DATE = DateTime.Now;
-                p.LAST_UPDATED_IP = Request.UserHostAddress;
-                db.SaveChanges();
+                proveedor.hab = false;
+
+                proveedor.LAST_UPDATED_BY = User.Identity.Name;
+
+                proveedor.LAST_UPDATED_DATE = DateTime.Now;
+
+                proveedor.LAST_UPDATED_IP = Request.UserHostAddress;
+
+                proveedorRepository.Remove(proveedor);
             }
             catch
             {
                 return RedirectToAction("Error406", "Error");
             }
+
             return RedirectToAction("Index", "Proveedor");
         }
 
-        [Authorize(Roles = "administrador, oficinatecnica, compras")]
-        [Route("/Proveedor/GetProveedores")]
         [HttpGet]
         public JsonResult GetProveedores(int desde, int hasta)
         {
-            var db = new OficinaTecnicaEntities();
-            var proveedores = from p in db.proveedores
-                              where p.hab == true
-                              select new { p.idProveedor, p.nombre, p.cuit, p.telefono, p.email };
+            var proveedores = proveedorRepository.Find(x => x.hab)
+                .Select(p => new { p.idProveedor, p.nombre, p.cuit, p.telefono, p.email });
 
-            return Json(new { Name = "/GetProveedores", Response = proveedores, Date = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss tt") }, JsonRequestBehavior.AllowGet);
+            return Json(new { Response = proveedores }, JsonRequestBehavior.AllowGet);
         }
 
-        [Authorize(Roles = "administrador, oficinatecnica, compras")]
-        [Route("/Proveedor/GetProveedores")]
         [HttpGet]
         public JsonResult GetProveedor(int IdProveedor)
         {
-            var db = new OficinaTecnicaEntities();
-            var proveedor = from p in db.proveedores
-                              where p.idProveedor == IdProveedor && p.hab == true
-                              select new { p.idProveedor, p.nombre, p.cuit, p.razonSocial, p.zona, p.direccion, p.telefono, p.email, p.horario, p.nombreContacto };
+            var proveedor = proveedorRepository.Find(x => x.idProveedor == IdProveedor && x.hab)
+                .Select(p => new { p.idProveedor, p.nombre, p.cuit, p.razonSocial, p.zona, p.direccion, p.telefono, p.email, p.horario, p.nombreContacto });
 
-            return Json(new { Name = "/GetProveedor", Response = proveedor, Date = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss tt") }, JsonRequestBehavior.AllowGet);
+            return Json(new { Response = proveedor }, JsonRequestBehavior.AllowGet);
         }
 
         [HttpGet]
