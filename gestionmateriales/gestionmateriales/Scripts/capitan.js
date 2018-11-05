6f3f4cb828ec51cc143112cdf1b4ded7dc45ed41
AgusstinnS <Agusstinn.Santich@gmail.com>
2018-11-05 12:33:34 -0300

    Menu laterial pantalla Ayuda

---
+++ gestionmateriales/gestionmateriales/Scripts/capitan.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+
+    $('.list-group-item').on('click', function () {
+        $('.fas', this)
+          .toggleClass('fas fa-angle-right')
+          .toggleClass('fas fa-angle-down');
+    });
+
+});
